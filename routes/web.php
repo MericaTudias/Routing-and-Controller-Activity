2c553af29adcbb2e8ad638b8cde70dcc0f288516
Merica Tudias <mericatudias33@gmail.com>
2024-02-14 13:41:27 +0800

    Creating Routes, Controllers and Views

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/aboutMe/{id}', [AboutMeController::class, 'show']);
+Route::get('/skills/{id}', [SkillsController::class, 'show']);
+Route::get('/hobbies/{id}', [HobbiesController::class, 'show']);
